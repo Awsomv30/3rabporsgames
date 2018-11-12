103ee4075ad2427ae011d65158797b3be9923574
Awsomv30 <44628705+Awsomv30@users.noreply.github.com>
2018-11-12 15:37:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,4 +209,23 @@ message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
   }
 });
 
+    client.on('message', message => {
+        var prefix = "+";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'ألامر')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
